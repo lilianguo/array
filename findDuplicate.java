bef9880ae6f3dafefb70b8b41508b1b0e6e8da43
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-08-31 21:28:48 -0700

    add find cycle in linkedlist and equal implementation in array

--- findDuplicate.java
+++ findDuplicate.java
@@ -30,4 +30,40 @@ class findDuplicate {
         }
         return -1;
     }
+    // Time complexity : O(n)
+    // Space complextiy: O(1)
+    /*
+    什么是快慢指针算法？
+    从起点出发，慢指针走一步，快指针走两步。因为有环，所以一定会相遇。
+    相遇之后，把其中一根指针拉回起点，重新走，这回快慢指针都各走一步。他们仍然会再次相遇，且相遇点为环的入口。
+
+    时间复杂度是多少？
+    时间复杂度是 O(n)O(n) 的。
+    */
+    public int findDuplicateIII(int[] nums) {
+        // Find the intersection point of the two runners.
+        int slow = nums[0];
+        int fast = nums[0];
+        System.out.println("slow is: " + slow);
+        System.out.println("fast is :" + fast);
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            System.out.println("slow is: " + slow);
+            System.out.println("fast is :" + fast);
+        } while (slow != fast);
+        
+        // Find the "entrance" to the cycle.
+        
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+            System.out.println("ptr1 is: " + ptr1);
+            System.out.println("ptr2 is :" + ptr2);
+        }
+
+        return ptr1;
+    }
 }
