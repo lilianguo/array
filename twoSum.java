ac9b6c48872d29181bde5f27f6ccf5b3c54ed3e7
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-03 23:34:23 -0700

    Update twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,18 @@
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums == null || nums.length < 2) {
+            return res;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
